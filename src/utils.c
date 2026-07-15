5b76fa9684bb20d1eae968d188a8d6cb53012389
leon <152042828+0xleonf@users.noreply.github.com>
2026-07-15 18:18:52 +0800

    it just prototype, still on-going:) (#2)
    hope it works

---
+++ src/utils.c
@@ -0,0 +1,11 @@
+#include "../include/utils.h"
+#include <math.h>
+#define PI 3.14159
+
+float sigma(int radius) { return 0.3f * (radius - 1) + 0.8f; }
+
+float gauss(int x, int radius) {
+  float s = sigma(radius);
+  float exponent = (x * x) / (2 * (s * s));
+  return (1 / (2 * PI * (s * s))) * exp(exponent);
+}

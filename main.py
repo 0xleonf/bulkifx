d7bac5449165baa8eb577fd2c1b42002449aac98
0xleonf <ucihasaskee07@gmail.com>
2026-09-02 10:42:48 +0800

    feat: use Sepia in Python CLI entrypoint

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 
-from ImageProc import Grayscale
+from ImageProc import Grayscale, Sepia
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit("usage: python3 main.py <input> <output>")
-    Grayscale(sys.argv[1], sys.argv[2])
+    # if len(sys.argv) != 3:
+    #     sys.exit("usage: python3 main.py <input> <output>")
+    # Grayscale(sys.argv[1], sys.argv[2])
+    input = "images.jpg"
+    Sepia(input, "output.png")

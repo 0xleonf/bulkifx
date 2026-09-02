552444859552daf20ea2991ae1b4cd08687dde87
0xleonf <ucihasaskee07@gmail.com>
2026-09-02 10:42:42 +0800

    feat: add sepia export binding in Go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 #cgo CFLAGS: -Iinclude
 #cgo LDFLAGS: -L${SRCDIR}/build -lbulkifx_static
 #include "include/bulkifx/grayscale/grayscale.h"
+#include "include/bulkifx/sepia/sepia.h"
 */
 import "C"
 
@@ -22,6 +23,19 @@ func Grayscaling(pathIn *C.char, pathOut *C.char) C.int {
 		return 1
 	}
 	return 0
+}
+
+//export Sepia
+func Sepia(pathIn *C.char, pathOut *C.char) C.int {
+	err := ImagePipeline(C.GoString(pathIn), C.GoString(pathOut), func(inPtr, outPtr *C.uint8_t, w, h int) {
+		C.c_sepia(inPtr, outPtr, C.int(w), C.int(h))
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "bulkifx: %v\n", err)
+		return 1
+	}
+	return 0
+
 }
 
 func main() {}
